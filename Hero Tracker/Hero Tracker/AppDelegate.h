6b3d0cffd5d8fefe2f2260b57c2fc4ae5d7805e7
Nick Perkins <nickperkins@protonmail.com>
2016-04-22 19:02:50 -0400

    start new project hero tracker

---
+++ Hero Tracker/Hero Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hero Tracker
+//
+//  Created by Nick Perkins on 4/22/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hero Tracker/Hero Tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hero Tracker
+//
+//  Created by Nick Perkins on 4/22/16.
+//  Copyright © 2016 Nick Perkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
